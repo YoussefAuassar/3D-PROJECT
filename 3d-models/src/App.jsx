889e9fc1a8e30911fcef204a375e6ac02ae75f0b
Youssef <youssef.auassar@student.ehb.be>
2024-10-24 15:18:27 +0200

    Importing 3d model in folder

--- 3d-models/src/App.jsx
+++ 3d-models/src/App.jsx
@@ -1,9 +1,15 @@
-import { useState } from "react";
-
+import { useState, Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { Environment, OrbitControls } from "@react-three/drei";
+import Horloge from "../public/Horloge";
 import "./App.css";
 
 function App() {
-	return <></>;
+	return (
+		<>
+			
+		</>
+	);
 }
 
 export default App;
