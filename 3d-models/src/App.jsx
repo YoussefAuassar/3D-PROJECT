784b4a5cb08f20eea39e550f715b6cae74018dc9
Youssef <youssef.auassar@student.ehb.be>
2024-10-26 15:29:27 +0200

    importing a second watch 3d asset

--- 3d-models/src/App.jsx
+++ 3d-models/src/App.jsx
@@ -2,21 +2,22 @@ import { useState, Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import { Environment, OrbitControls } from "@react-three/drei";
 import Horloge from "../public/Horloge";
+import Watch from "../public/Watch";
 import "./App.css";
 
 function App() {
-	return (
-		<>
-			<Canvas>
-				<ambientLight intensity={1} />
-				<OrbitControls  />
-				<Suspense fallback={null}>
-					<Horloge />
-				</Suspense>
-				<Environment preset="sunset" />
-			</Canvas>
-		</>
-	);
+    return (
+        <>
+            <Canvas>
+                <ambientLight intensity={1} />
+                <OrbitControls />
+                <Suspense fallback={null}>
+                    <Horloge position={[0, 0, 0]} scale={[8, 8, 8]} /> 
+                </Suspense>
+                <Environment preset="sunset" />
+            </Canvas>
+        </>
+    );
 }
 
 export default App;
